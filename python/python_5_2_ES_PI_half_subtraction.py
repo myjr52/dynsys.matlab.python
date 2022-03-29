bc31ec8c9ff8b78c7bafcd718816b9f1a4422929
Jongrae.K <myjr52@gmail.com>
2022-03-29 08:56:34 +0100

    Add files via upload

--- python/python_5_2_ES_PI_half_subtraction.py
+++ python/python_5_2_ES_PI_half_subtraction.py
@@ -24,6 +24,9 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
+import numpy as np
+from scipy.integrate import solve_ivp
+
 # parameters
 kP = 50
 kI = 5e-6
@@ -53,72 +56,16 @@ time_current = 0       # initial time
 time_final   = 3600*16 # final time [min]
 tspan = [time_current, time_final]
 
-
-%% simulation
-para = [kP kI gamma_G ks2 kon kcat kdg ks1 Pd KF Kdeg X3 eta time_for_step_change];
-ode_option = odeset('RelTol',1e-3,'AbsTol',1e-6);
-state_t0    = 0.1*ones(1,8); 
-state_t0(3) = E_total - state_t0(1);
-state_t0(4) = 1e-3; 
-state_t0(5) = 1e-3;
-sol = ode15s(@(time, state)ES_PI_Half_Subtraction(time, state, para),tspan, ...
-    state_t0, ode_option);
-
-%----------------- Change Here------------------------------
-figure; 
-clf;
-subplot(211);
-time_hr = sol.x/3600; % [hour]
-P_history = sol.y(2,:); 
-
-%plot(time_hr,Pd*ones(size(time_hr)),'r--');
-plot([0:time_for_step_change time_for_step_change:time_final/3600], ...
-     [ones(1,9)*Pd ones(1,9)*Pd/2],'r--');
-
-hold on;
-plot(time_hr,P_history,'b');
-set(gca,'FontSize',14);
-ylabel('[P(t)] [a.u.]');
-legend('desired [P]', 'achieved [P]');
-axis([0 time_final/3600 0 1.2]);
-subplot(212);
-plot(sol.x/3600,sol.y(6,:));
-set(gca,'FontSize',14);
-xlabel('time [hour]');
-ylabel('[S] [a.u.]');
-axis([0 time_final/3600, 0 120]);
-
-%% E-S PI Control Half Subtraction
-function dxdt = ES_PI_Half_Subtraction(time,state,ki_para)
-    E   = state(1);
-    P   = state(2);
-    ES  = state(3);
-    X1  = state(4);
-    X2  = state(5);
-    S   = state(6);
-    DP  = state(7);
-    Xs  = state(8);
+# E-S PI Control Half Subtraction
+def ES_PI_Half_Subtraction(time,state,ki_para):
+    E, P, ES, X1, X2, S,DP, Xs   = state
     
-    kP = ki_para(1);
-    kI = ki_para(2);
-    gamma_G = ki_para(3);
-    ks2 = ki_para(4);
-    kon = ki_para(5);
-    kcat = ki_para(6);
-    kdg = ki_para(7);
-    ks1 = ki_para(8);
-    Pd = ki_para(9);
-    KF = ki_para(10);
-    Kdeg = ki_para(11);
-    X3 = ki_para(12);
-    eta = ki_para(13);
-    time_for_step_change = ki_para(14);
+    kP, kI, gamma_G, ks2, kon, kcat, kdg, ks1, Pd, KF, Kdeg, X3, eta, time_for_step_change = ki_para
     
-    if time > 3600*time_for_step_change
-        Pd = Pd/2;
-    end
-    
-    Pd = Pd*KF; % Pd scaling
+    if time > 3600*time_for_step_change:
+        Pd = Pd/2
+        
+    Pd = Pd*KF
     
     dE_dt = -kon*E*S + kcat*ES;
     dP_dt = kcat*ES - kdg*P - Kdeg*X3*P;
@@ -129,7 +76,36 @@ function dxdt = ES_PI_Half_Subtraction(time,state,ki_para)
     dDP_dt = ks1*Pd - ks1*DP*Xs - ks1*DP;
     dXs_dt = -ks1*DP*Xs + ks1*P;
     
-    dxdt = [dE_dt; dP_dt; dES_dt; dX1_dt; dX2_dt; dS_dt; dDP_dt; dXs_dt];
-    
-    
-end
+    dxdt = [dE_dt, dP_dt, dES_dt, dX1_dt, dX2_dt, dS_dt, dDP_dt, dXs_dt]
+    return dxdt
+
+
+# simulation
+para = [kP, kI, gamma_G, ks2, kon, kcat, kdg, ks1, Pd, KF, Kdeg, X3, eta, time_for_step_change]
+state_t0    = 0.1*np.ones(8) 
+state_t0[2] = E_total - state_t0[0]
+state_t0[3] = 1e-3 
+state_t0[4] = 1e-3
+
+sol = solve_ivp(ES_PI_Half_Subtraction, (time_current,time_final), state_t0, args=(para,))
+
+tout = sol.t
+xout = sol.y
+
+import matplotlib.pyplot as plt
+
+figure, (ax0,ax1) = plt.subplots(2,1)
+time_hr = tout/3600 # [hour]
+P_history = xout[1,:]
+
+ax0.plot([0, time_for_step_change,time_for_step_change, time_final/3600],
+         [Pd, Pd, 0.5*Pd, 0.5*Pd], 'r--')
+ax0.plot(time_hr, P_history,'b')
+ax0.set_ylabel('[P(t)] [a.u.]')
+ax0.legend(('desired [P]','achieved [P]'))
+ax0.axis([0,time_final/3600,0,1.2])
+
+ax1.plot(time_hr, xout[5,:])
+ax1.set_xlabel('time [hour]')
+ax1.set_ylabel('[S] [a.u.]')
+ax1.axis([0, time_final/3600, 0, 120])
